b045584a34b95d9572ce3e0c85329890c0484af4
Dazer Studios <59792880+dazerstudio@users.noreply.github.com>
2022-11-25 15:14:59 +0100

    Add files via upload
    Im working on an install script for linux,
    It will allow to visit blocked sites,
    And the school wont be able to see what sites are open

---
+++ Project COOL.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "Installing Project COOL!"
+echo "Updating System"
+sudo apt update
+sudo apt upgrade -y
+echo "Done, updating!"
+echo "Installing Brave (Chrome but better)"
+sudo apt install apt-transport-https curl -y
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update
+sudo apt install brave-browser -y
+echo "Done installing Brave"
+echo "Installing Discord"
+curl https://discord.com/api/download?platform=linux&format=deb --output discord.deb
+sudo apt install ./discord.deb -y
+rm discord.deb
+echo "Done installing Discord"
+echo "Done installing Project COOL!"
+echo "Removing install script"
+rm install.sh
