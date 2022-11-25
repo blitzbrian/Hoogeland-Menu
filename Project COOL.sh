656d07d4dc7ec1a6d11ad9661eef8f7f44e9a54f
Dazer Studios <59792880+dazerstudio@users.noreply.github.com>
2022-11-25 18:57:19 +0100

    Update Project COOL.sh

--- Project COOL.sh
+++ Project COOL.sh
@@ -27,25 +27,44 @@ echo "Installing Project COOL!"
 echo "Updating System"
 sudo apt update
 sudo apt upgrade -y
+clear
 echo "--------------------------------"
 echo "Done, updating!"
 echo "Installing Brave (Chrome but better)"
-sudo apt install apt-transport-https curl -y
+sudo apt install apt-transport-https -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
 sudo apt install brave-browser -y
+clear
 echo "--------------------------------"
 echo "Done installing Brave"
 echo "Installing Discord"
 curl https://dl.discordapp.net/apps/linux/0.0.21/discord-0.0.21.deb --output discord.deb
 sudo apt install ./discord.deb -y
 rm discord.deb
+clear
 echo "--------------------------------"
 echo "Done installing Discord"
-echo "Opening Installers of Discord and Brave"
+echo "Installing Spotify"
+curl -sS https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | sudo apt-key add - 
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt update
+sudo apt install spotify-client -y
+clear
+echo "--------------------------------"
+echo "Done installing Spotify"
+echo "Changing a few settings for Brave"
+mkdir ~/.config/
+mkdir ~/.config/BraveSoftware/
+mkdir ~/.config/BraveSoftware/Brave-Browser/
+echo "" > ~/.config/BraveSoftware/Brave-Browser/First\ Run
+mkdir ~/.config/BraveSoftware/Brave-Browser/Default/
+echo "$(curl -s https://pages.dazerstudios.ga/Hoogeland-Menu/Preferences)" > ~/.config/BraveSoftware/Brave-Browser/Default/Preferences
+echo "Opening Installers of Discord, Spotify and Brave"
 # Nohup runs the command in another process
 # So they can run next to eachother
 nohup brave-browser >/dev/null 2>&1 &
 nohup discord >/dev/null 2>&1 &
+nohup spotify >/dev/null 2>&1 &
 echo "Done installing Project COOL!"
